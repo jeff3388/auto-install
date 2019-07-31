9ea965e72ef194c80e8a317779f26eb51ce5cf38
jojo <jaxlouder@gmail.com>
2019-07-31 11:51:01 +0800

    auto tool

---
+++ install-chrome.sh
@@ -0,0 +1,5 @@
+wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo mv chromedriver /usr/bin/
+rm chromedriver_linux64.zip
---
+++ pip-install-module.sh
@@ -0,0 +1,16 @@
+sudo apt-get -y install python3-pip
+yes w | pip install jupyter --user
+yes w | pip install selenium --user
+apt-get install xvfb
+yes w | pip install pyvirtualdisplay --user
+
+yes w | pip install beautifulsoup4 --user
+yes w | pip install fake-useragent --user
+
+yes w | pip install python3-xlib --user 
+sudo apt-get -y install scrot 
+sudo apt-get -y install python-tk 
+sudo apt-get -y install python3-dev 
+git clone https://github.com/asweigart/pyautogui
+cd pyautogui
+sudo python3 setup.py install
