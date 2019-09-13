9c03b50a03bab3b01ed3d4a1760428dea930ad82
jeff3388 <35676334+jeff3388@users.noreply.github.com>
2019-09-13 21:34:15 +0800

    Update install-anaconda.sh

--- install-anaconda.sh
+++ install-anaconda.sh
@@ -1,3 +1,4 @@
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh
-source ~/.bashrc
+echo 'export PATH="~/anaconda3/bin:$PATH"' >> ~/.bashrc
+source .bashr
